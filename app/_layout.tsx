a591670889cb6627e9e005b295b9d8e89ad2656b
neelypan <neel.pandruvada@gmail.com>
2025-09-29 14:20:58 -0400

    ayaans doing something

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,38 @@
 import { Stack } from "expo-router";
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const _layout = () => {
+  return (
+    <Tabs tabBar={(props) => <TabBar {...props} />}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="cardio"
+        options={{
+          title: "Cardio",
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+        }}
+      />
+    </Tabs>
+  );
+};
 
 export default function RootLayout() {
   return <Stack />;
---
+++ app/cardio.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Cardio = () => {
+  return (
+    <View>
+      <Text>Cardio</Text>
+    </View>
+  );
+};
+
+export default Cardio;
